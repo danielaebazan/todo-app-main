eda116de364f0e66b99a584fdfc783fa0163b05f
danielaebazan <daniela.e.bazan@hotmail.com>
2024-03-19 16:40:49 -0600

    background and ligth/dark themes funcionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
+import React from 'react';
+import './index.css'; 
 import ListHeader from './components/ListHeader';
-
-
+import Theme from './components/Theme'; 
 
 const App = () => {
-  
-  
   return (
     <div className="app">
-      <ListHeader listName = {'TO DO'}/>
+      <ListHeader listName={'TO DO'} />
+      {/* Render the Theme component */}
+      <Theme />
     </div>
   );
 }
 
 export default App;
+
--- client/src/components/Theme.js
+++ client/src/components/Theme.js
@@ -1,7 +1,20 @@
+import React, { useState } from 'react';
+import '../index.css'; 
+import moonIcon from '../images/icon-moon.svg'; 
+import sunIcon from '../images/icon-sun.svg'; 
 
 const Theme = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
+
   return (
-    <div>
+    <div className={`app ${isDarkTheme ? 'dark-theme' : 'light-theme'}`}>
+      <button className="theme-toggle" onClick={toggleTheme}>
+        <img src={isDarkTheme ? sunIcon : moonIcon} alt={isDarkTheme ? 'Sun' : 'Moon'} />
+      </button>
     </div>
   );
 }
