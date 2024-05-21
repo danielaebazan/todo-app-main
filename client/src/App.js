4b0e627551837d0d346c75f6fbce8a39f9c75dd8
danielaebazan <daniela.e.bazan@hotmail.com>
2024-05-21 17:07:56 -0600

    frontend and backend connected

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './index.css'; 
 import ListHeader from './components/ListHeader';
 import Theme from './components/Theme'; 
 
+
+
 const App = () => {
+
+  const getData = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/todos`)
+      const json = await response.json()
+      console.log(json)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => getData, [])
+
   return (
     <div className="app">
       <ListHeader listName={'TO DO'} />
