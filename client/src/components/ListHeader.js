34ee15cb53c31c1659c8aa92ee634934c5f3203e
danielaebazan <daniela.e.bazan@hotmail.com>
2024-05-29 18:18:15 -0600

    starting form

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,10 +1,17 @@
+import Modal from "./Modal";
 
 const ListHeader = ( {listName} ) => {
   return (
     <div className="list-header">
       <h1>{listName}</h1>
+      
+      <div className="info-container">
+       <Modal/>
+      </div>
+     
     </div>
-  );
+  
+  )
 }
 
 export default ListHeader;
--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,7 +1,27 @@
 
 const Modal = () => {
+  
+  const handleChange = () => {
+    console.log('changing')
+  }
+  
   return (
     <div>
+
+    <form className="modal">
+      <input
+      required
+      maxLength={30}
+      placeholder=" Create a new todo..."
+      name="title"
+      value={""}
+      onChange={handleChange}
+    />
+    <br/>
+  
+    <input className="tick create" type="submit"/>
+    </form>
+
     </div>
   );
 }
