dc739df92c2fb92384026dd99118fd925befd6ef
danielaebazan <daniela.e.bazan@hotmail.com>
2024-07-20 22:58:47 -0600

    vercel supabase

--- server/db.js
+++ server/db.js
@@ -7,7 +7,7 @@ const pool = new Pool({
     password: process.env.PASSWORD,
     host: process.env.HOST,
     port: process.env.DBPORT,
-    database: 'todoappmain'
+    database: 'postgres'
 })
 
 module.exports = pool
